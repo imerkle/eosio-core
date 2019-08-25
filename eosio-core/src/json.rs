dc32fc5cd84822308ccf60fd8ba91dac5150bc94
imerkle <dsslimshaddy@gmail.com>
2019-08-26 00:36:23 +0530

    first commit

---
+++ eosio-core/src/json.rs
@@ -0,0 +1,42 @@
+//! TODO docs
+use serde::{de, Deserialize, Deserializer, Serializer};
+
+/// TODO docs
+#[inline]
+pub fn bool_from_u8<'de, D>(deserializer: D) -> Result<bool, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: u8 = Deserialize::deserialize(deserializer)?;
+    Ok(s == 1)
+}
+
+/// TODO docs
+#[inline]
+pub fn bool_to_u8<S>(x: &bool, s: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let num = if *x { 1 } else { 0 };
+    s.serialize_u8(num)
+}
+
+/// TODO docs
+#[inline]
+pub fn f64_from_string<'de, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    s.parse().map_err(de::Error::custom)
+}
+
+/// TODO docs
+#[inline]
+pub fn u64_from_string<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    s.parse().map_err(de::Error::custom)
+}
